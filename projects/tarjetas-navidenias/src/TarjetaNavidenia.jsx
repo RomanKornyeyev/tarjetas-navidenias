19e277b008a35f3dbf2f430779bbe34d90913b22
SrGalleto <romankr1997@gmail.com>
2025-01-01 15:47:39 +0100

    tarjetas navideñas mierdecilla inicial

---
+++ projects/tarjetas-navidenias/src/TarjetaNavidenia.jsx
@@ -0,0 +1,8 @@
+export function TarjetaNavidenia ({ title, text }) {
+    return(
+        <div className="card-global">
+            <h1 className="card-title">{ title }</h1>
+            <p className="card-text">{ text }</p>
+        </div>
+    )
+}
